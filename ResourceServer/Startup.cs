bd3f04091e8c2498e7d43077a4697ffc5b22699f
jayrgee <jayrgee@fastmail.fm>
2014-11-27 21:50:42 +1100

    added resource server

---
+++ ResourceServer/Startup.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using TokenAuthentication.ResourceServer.App_Start;
+
+[assembly: OwinStartup(typeof(TokenAuthentication.ResourceServer.Startup))]
+
+namespace TokenAuthentication.ResourceServer
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            ConfigureOAuth(app);
+
+            WebApiConfig.Register(config);
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseWebApi(config);
+            
+        }
+
+        private void ConfigureOAuth(IAppBuilder app)
+        {
+            //Token Consumption
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions
+            {
+            });
+        }
+    }
+}
